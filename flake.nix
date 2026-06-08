cdcb8f6837847a9135d550c6d9deeb9d0f32a3bb
Edgar Albalate <dev.gardo@gmail.com>
2026-06-08 13:47:08 +0200

    fix: enables flake for linux systems

--- flake.nix
+++ flake.nix
@@ -3,18 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          bun
-          yt-dlp
-        ];
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            bun
+            yt-dlp
+          ];
+        };
+      });
 }
